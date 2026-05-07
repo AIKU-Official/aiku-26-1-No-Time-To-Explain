a3560fb9a968986a89f5bdd2d492783199bd1e7d
kukyoung <kukyoung830@naver.com>
2026-05-07 15:27:23 +0900

    making basket bars

---
+++ config.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+# date format: '%Y-%m-%d'
+TRAIN_START_DATE = "2014-01-06"
+TRAIN_END_DATE = "2020-07-31"
+
+TEST_START_DATE = "2020-08-01"
+TEST_END_DATE = "2021-10-01"
+
+TRADE_START_DATE = "2021-11-01"
+TRADE_END_DATE = "2021-12-01"
+
+INDICATORS = [
+    "macd",
+    "boll_ub",
+    "boll_lb",
+    "rsi_30",
+    "cci_30",
+    "dx_30",
+    "close_30_sma",
+    "close_60_sma",
+]
